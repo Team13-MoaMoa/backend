69d509ae4078b0b880852a01d9b2ab6f3daf28fd
jiminsong490 <misosjm@naver.com>
2023-04-29 01:35:47 +0900

    refactor : @LoginUser 적용

--- src/main/java/sku/moamoa/domain/comment/controller/CommentController.java
+++ src/main/java/sku/moamoa/domain/comment/controller/CommentController.java
@@ -9,7 +9,7 @@ import sku.moamoa.domain.comment.service.CommentService;
 import sku.moamoa.domain.post.entity.Post;
 import sku.moamoa.domain.post.service.PostService;
 import sku.moamoa.domain.user.entity.User;
-import sku.moamoa.domain.user.service.UserService;
+import sku.moamoa.global.annotation.LoginUser;
 import sku.moamoa.global.result.ResultResponse;
 
 import static sku.moamoa.global.result.ResultCode.*;
@@ -20,12 +20,10 @@ import static sku.moamoa.global.result.ResultCode.*;
 @RequestMapping("/api/v1/comments")
 public class CommentController {
     private final CommentService commentService;
-    private final UserService userService;
     private final PostService postService;
 
     @PostMapping("/{pid}")
-    public ResponseEntity<ResultResponse> createComment(@PathVariable Long pid, @RequestBody CommentDto.CreateRequest body) {
-        User user = userService.findById(body.getUserId());
+    public ResponseEntity<ResultResponse> createComment(@PathVariable Long pid, @RequestBody CommentDto.CreateRequest body, @LoginUser User user) {
         Post post = postService.findById(pid);
         commentService.registerComment(user, post, body);
         return ResponseEntity.ok(ResultResponse.of(COMMENT_CREATE_SUCCESS));
