1bf77b70a349be52113f7b6ceeb0920b9bfdfd46
jiminsong490 <misosjm@naver.com>
2023-04-29 01:22:21 +0900

    feat : @LoginUser 어노테이션을 사용하기 위한 config, resolver 생성

--- src/main/java/sku/moamoa/global/config/WebConfig.java
+++ src/main/java/sku/moamoa/global/config/WebConfig.java
@@ -1,10 +1,19 @@
 package sku.moamoa.global.config;
 
 import io.swagger.models.HttpMethod;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import sku.moamoa.global.resolver.LoginUserArgumentResolver;
 
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -12,6 +21,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods(HttpMethod.GET.name(),HttpMethod.POST.name(), HttpMethod.PUT.name(),
                         HttpMethod.PATCH.name(),HttpMethod.OPTIONS.name(),HttpMethod.DELETE.name())
                 .allowCredentials(false).maxAge(3600);
+    }
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
     }
 
 }
