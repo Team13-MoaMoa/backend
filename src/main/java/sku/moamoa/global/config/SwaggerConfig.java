5bddadf8b94532f04c9246858cef03110083de05
jiminsong490 <misosjm@naver.com>
2023-05-30 01:34:34 +0900

    feat : swagger에서 @Login parameter ignore

--- src/main/java/sku/moamoa/global/config/SwaggerConfig.java
+++ src/main/java/sku/moamoa/global/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package sku.moamoa.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import sku.moamoa.global.annotation.LoginUser;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -32,6 +33,7 @@ public class SwaggerConfig {
     @Bean
     public Docket swaggerApi() {
         return new Docket(DocumentationType.OAS_30)
+                .ignoredParameterTypes(LoginUser.class)
                 .securityContexts(Arrays.asList(securityContext())) // 추가
                 .securitySchemes(Arrays.asList(apiKey()))
                 .consumes(getConsumeContentTypes())
@@ -39,6 +41,7 @@ public class SwaggerConfig {
                 .apiInfo(swaggerInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("sku.moamoa"))
+
                 .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(false);
