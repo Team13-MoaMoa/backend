b10f6223823f2f1de51d4483bfe987500c8053f1
jiminsong490 <misosjm@naver.com>
2023-06-15 21:51:04 +0900

    chore : restdocs 테스트 작성 전 세팅

---
+++ src/test/java/sku/moamoa/global/entity/BaseTestEntity.java
@@ -0,0 +1,37 @@
+package sku.moamoa.global.entity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import sku.moamoa.global.config.RestDocsConfig;
+
+@Import(RestDocsConfig.class)
+@ExtendWith(RestDocumentationExtension.class)
+public class BaseTestEntity {
+    @Autowired
+    MockMvc mvc;
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(
+            final WebApplicationContext context,
+            final RestDocumentationContextProvider provider
+    )
+    {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context) // mvc 설정
+                .apply(MockMvcRestDocumentation.documentationConfiguration(provider)) // restDocs 설정
+                .alwaysDo(restDocs)
+                .build();
+    }
+}
